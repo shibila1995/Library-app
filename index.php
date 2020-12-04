569ae7844526b5dd5bff15c08ee68479e14b6804
shibila1995 <shibilanaf@gmail.com>
2020-12-04 17:04:40 +0400

    edited with css

--- index.php
+++ index.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>library</title>
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
-<body>
+<body class = "backcolour">
     <div class="container">
         <div class="row">
             <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                <h1>book details</h1>
+               <center><h1>book details</h1></center> 
                 <table class="table">
                     <tr>
                         <td>name</td>
